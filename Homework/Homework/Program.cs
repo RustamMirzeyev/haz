90045d496f119e0b38c5cda8a6d95617c0841254
Rustam Mirzoev <74654492+RustamMirzeyev@users.noreply.github.com>
2020-12-01 01:32:55 -0800

    Add files via upload

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder word = new StringBuilder("Dark in da House ");
+            Console.WriteLine(word);
+
+        }
+
+        static StringBuilder dark(string word)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                //Console.WriteLine(word[i]);
+                //result.Append(word[i]);
+
+
+            }
+
+            return result;
+        }
+    }
+}
